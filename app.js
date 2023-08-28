45be0fe070cb0c18db921ff6bf1c11cf3a47210e
Al Tomizawa <alyssontomizawa@hotmail.com>
2023-08-28 19:36:35 -0300

    server up

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const express = require("express");
 const { PORT = 3000 } = process.env;
 
 const app = express();
+const {users} = require('./users.js')
+
+app.get('/', (req, res) =>{
+  res.send('<h1>hello, World</h1>')
+})
+
+app.get('/users', (req, res) => {
+  res.send('these are all the users,')
+  
+})
 
 app.listen(PORT, () => {
   console.log("The app is listeening on PORT 300");
