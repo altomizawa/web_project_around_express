9200ec12ebf3ace3a00b74a9c3a67ba0252d965c
Al Tomizawa <alyssontomizawa@hotmail.com>
2023-09-03 12:48:26 -0300

    fix reading json files

--- routes/cards.js
+++ routes/cards.js
@@ -2,13 +2,15 @@ const router = require("express").Router();
 const path = require("path");
 const fsPromises = require("fs").promises;
 
-const cardRoute = path.join(__dirname, "data/cards.json");
-console.log(cardRoute);
+const cardRoute = path.join(__dirname, "../data/cards.json");
 
 router.get("/", (req, res) => {
-  fsPromises.readFile(cardRoute).then((data) => {
-    res.send(data);
-  });
+  fsPromises.readFile(cardRoute, 'utf8')
+  .then(cards => res.send(cards))
+  .catch((err)=>{
+    console.error(err);
+    res.status(500).send('Error reading the file')
+  })
 });
 
 router.get("/:");
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
+const path = require('path');
+const fsPromises = require('fs').promises
+
+const userRoute = path.join(__dirname, '../data/users.json')
 
 router.get("/", (req, res) => {
-  res.send("users");
+  fsPromises.readFile(userRoute, 'utf8')
+  .then(users => res.send(users))
+  .catch((err)=>{
+    console.error(err);
+    res.status(500).send('Error reading the file')
+  })
 });
 
 module.exports = router;
