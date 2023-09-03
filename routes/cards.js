f856f21e0a33dd4f692f409efb528a55b6d992a5
altomizawa <alyssontomizawa@hotmail.com>
2023-09-03 14:09:42 -0300

    fix user id get

--- routes/cards.js
+++ routes/cards.js
@@ -5,13 +5,13 @@ const fsPromises = require("fs").promises;
 const cardRoute = path.join(__dirname, "../data/cards.json");
 
 router.get("/", (req, res) => {
-  fsPromises.readFile(cardRoute, 'utf8')
-  .then(cards => res.send(cards))
-  .catch((err)=>{
-    console.error(err);
-    res.status(500).send('Error reading the file')
-  })
+  fsPromises
+    .readFile(cardRoute, "utf8")
+    .then((cards) => res.send(JSON.parse(cards)))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error reading the file");
+    });
 });
 
-router.get("/:");
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,37 @@
 const router = require("express").Router();
-const path = require('path');
-const fsPromises = require('fs').promises
+const path = require("path");
+const fsPromises = require("fs").promises;
 
-const userRoute = path.join(__dirname, '../data/users.json')
+const userRoute = path.join(__dirname, "../data/users.json");
 
 router.get("/", (req, res) => {
-  fsPromises.readFile(userRoute, 'utf8')
-  .then(users => res.send(users))
-  .catch((err)=>{
-    console.error(err);
-    res.status(500).send('Error reading the file')
-  })
+  fsPromises
+    .readFile(userRoute, { encoding: "utf8" })
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error reading the file");
+    });
+});
+
+router.get("/:id", (req, res) => {
+  fsPromises
+    .readFile(userRoute, "utf8")
+    .then((users) => {
+      const parsedUsers = JSON.parse(users);
+      const user = parsedUsers.find((user) => user._id === req.params.id);
+      if (!user) {
+        res.status(404).send({ message: "Usuário não encontrado" });
+      } else {
+        res.send({ data: user });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error reading the file");
+    });
 });
 
 module.exports = router;
