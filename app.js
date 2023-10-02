a6e625db27bb5b70466b173bb1a68842fc6d9cff
altomizawa <alyssontomizawa@hotmail.com>
2023-10-01 21:06:56 -0300

    empty

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use("/users", userRouter);
 app.use("/cards", cardRouter);
 
 app.get("/", (req, res) => {
-  res.send("<h1>Server listening on PORT 3000</h1>");
+  res.send("<h1>Server listening on PORT 3000.</h1>");
 });
 
 // Middleware to handle invalid routes
